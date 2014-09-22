1ed400cf913000139c2cd13e7980a3f5f7b7df95
elyh31 <elyh31@unitec.edu>
2014-09-22 07:47:45 -0600

    tarea 5

--- src/Ejercicios.java
+++ src/Ejercicios.java
@@ -4,18 +4,21 @@ public class Ejercicios
 	//Devuelve el valor de mi_arreglo en la posicion [columna][fila]
 	static String obtenerValor(String mi_arreglo[][], int columna, int fila)
 	{
+		//va a devolver el areeglo en la posicion de la columna y fila
 		return mi_arreglo[columna][fila];
 	}
 	
 	//Devuelve la cantidad de columnas de mi_arreglo
 	static int obtenerCantidadColumnas(double mi_arreglo[][])
 	{
+		//ayuda a devoelver la cantidad de columnas del arreglo
 		return mi_arreglo.length;
 	}
 	
 	//Devuelve la cantidad de filas de mi_arreglo
 	static int obtenerCantidadFilas(double mi_arreglo[][])
 	{
+		//ayuda a devoelver la cantidad de filas del arreglo
 		return mi_arreglo[1].length;
 	}
 	
@@ -23,9 +26,12 @@ public class Ejercicios
 	static int obtenerCantidadElementos(double mi_arreglo[][])
 	{
 		double suma = 0;
+		//el ciclo me ayuda a correr todo el arreglo
 		for(int c=0; c<mi_arreglo.length; c++)
-		for(int f=0; f<mi_arreglo[1].length; f++)	
+		for(int f=0; f<mi_arreglo[1].length; f++)
+		//al correr todo el arreglo se multiplica la cantidad de filas por la cantidad de columnas
 			suma = c * f;
+		//me devuelve la multiplicacion en total
 		return (int)suma;
 	}
 	
@@ -33,14 +39,16 @@ public class Ejercicios
 	static double sumaElementos(double mi_arreglo[][])
 	{
 		double suma = 0;
+		//el ciclo me ayuda a correr todo el arreglo
 		for(int c=0; c<mi_arreglo.length; c++)
 		{
 			for(int f=0; f<mi_arreglo[1].length; f++)
 			{
+				//me ayuda a saber cuantos suman los valores de todos los arreglos
 				suma = suma + mi_arreglo[c][f];
 			}
 		}
-					
+		//devuelde la cantidad de los valores de todos los arreglos			
 		return suma;
 	}
 	
@@ -48,11 +56,14 @@ public class Ejercicios
 	static double promedioElementos(double mi_arreglo[][])
 	{
 		double suma = 0;
+		//ayuda a saber cuantos arreglos hay en total
 		double promedio = mi_arreglo.length * mi_arreglo[1].length;
+		//el ciclo me ayuda a correr todo el arreglo
 		for(int c=0; c<mi_arreglo.length; c++)
 		{
 			for(int f=0; f<mi_arreglo[1].length; f++)
 			{
+				//me ayuda a saber cuantos suman los valores de todos los arreglos
 				suma = suma + mi_arreglo[c][f];
 			}
 		}
@@ -63,15 +74,17 @@ public class Ejercicios
 	//Devuelve verdadero si num esta en mi_arreglo, de lo contrario devuelve false
 	static boolean existe(double mi_arreglo[][], int num)
 	{
+		//el ciclo me ayuda a correr todo el arreglo
 		for(int c=0; c<mi_arreglo.length; c++)
 		{
 			for(int f=0; f<mi_arreglo[1].length; f++)
 			{
+				// me devuelve verdadero si el valor existe en algun arreglo
 				if (mi_arreglo[c][f]==num);
 				return true;
 			}
 		}
-					
+		// me devuelve falso si el valor no existe en algun arreglo			
 		return false;
 	}
 	
